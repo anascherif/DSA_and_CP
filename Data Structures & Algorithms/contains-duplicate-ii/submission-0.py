8bce44afc854c6705b944e2c0fb084a416d57614
CHERIF ANES ABD EL MALEK <anesabdelmalek.cherif@insat.ucar.tn>
2026-09-03 18:08:02 +0100

    Add: contains-duplicate-ii - submission-0

---
+++ Data Structures & Algorithms/contains-duplicate-ii/submission-0.py
@@ -0,0 +1,11 @@
+class Solution:
+    def containsNearbyDuplicate(self, nums: List[int], k: int) -> bool:
+        i=0
+        while (i<len(nums)):
+            j=i+1
+            while j<=i+k and j<len(nums):
+                if nums[j]==nums[i]:
+                    return True
+                j+=1
+            i+=1
+        return False
