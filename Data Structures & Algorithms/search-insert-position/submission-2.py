b2f9cc304c948e45ed593811067795377af126e1
CHERIF ANES ABD EL MALEK <anesabdelmalek.cherif@insat.ucar.tn>
2026-09-06 22:58:53 +0100

    Add: search-insert-position - submission-2

---
+++ Data Structures & Algorithms/search-insert-position/submission-2.py
@@ -0,0 +1,15 @@
+class Solution:
+    def searchInsert(self, nums: List[int], target: int) -> int:
+        i,j=0,len(nums)-1
+        if target<nums[i]:
+            return 0
+        
+        while(i<=j):
+            mid=(i+j)//2
+            if target<nums[mid]:
+                j=mid-1
+            elif target>nums[mid]:
+                i=mid+1
+            else:
+                return mid
+        return i
