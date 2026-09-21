592bbe99b0162f008a8aab679bc8633b52fa7c6f
CHERIF ANES ABD EL MALEK <anesabdelmalek.cherif@insat.ucar.tn>
2026-09-21 21:54:29 +0100

    Add: balanced-binary-tree - submission-0

---
+++ Data Structures & Algorithms/balanced-binary-tree/submission-0.py
@@ -0,0 +1,22 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+class Solution:
+    def isBalanced(self, root: Optional[TreeNode]) -> bool:
+        def h(node):
+            if not node: 
+                return 0
+            right=h(node.right)
+            if right==-1:return -1
+            left=h(node.left)
+            if left==-1:return -1
+            if abs(left-right)>1:
+                return -1
+            
+            return 1+max(right,left)
+        return h(root)!=-1
+        
